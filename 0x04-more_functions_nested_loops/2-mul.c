aa16427b31297613345de04b18ef956465e1c71d
Vidya-Naidoo <vidya.naidoo82@gmail.com>
2023-02-22 23:54:08 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiples of two
+ * @x: The first number
+ * @y: The second number
+ * Return: The result of multiplication
+ */
+
+int mul(int x, int y)
+{
+	return (x * y);
+}
