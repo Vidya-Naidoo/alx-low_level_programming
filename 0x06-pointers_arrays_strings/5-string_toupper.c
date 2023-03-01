4e6c8fa318e1e0f6e812ce788bd22642bc869f2b
Vidya-Naidoo <vidya.naidoo82@gmail.com>
2023-03-01 05:58:48 -0800

    Task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters
+ * @str: The string
+ * Return: A pointer
+ */
+
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+	if (str[index] >= 'a' && str[index] <= 'z')
+	str[index] -= 32;
+	index++;
+	}
+	return (str);
+}
